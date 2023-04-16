cf298468d4385c2291f958de3c91f3fc2c6790cc
yinchen <yinchenwang1998@gmail.com>
2023-04-16 17:02:07 +0200

    all function

--- static/assets/js/my.js
+++ static/assets/js/my.js
@@ -2,20 +2,20 @@
 // 	window.location.href = "service.html"
 // })
 
-function addcomme(){
-		var value= document.getElementById("com");
-		var p= document.createElement("p");
-		p.innerHTML='<hr>'+value.value;
-		document.getElementById("comArea").prepend(p); 
-		value.value=null;
-}
-let num = 0 
-function like(){
-	let a = document.getElementById('like')
-	num += 1;
-	a.innerText = num + " likes";
-}
-
-function addFavo(){
-	alert("Il est bien ajouté dans votre list de favorable.")
-}
+// function addcomme(){
+// 		var value= document.getElementById("com");
+// 		var p= document.createElement("p");
+// 		p.innerHTML='<hr>'+value.value;
+// 		document.getElementById("comArea").prepend(p);
+// 		value.value=null;
+// }
+// let num = 0
+// function like(){
+// 	let a = document.getElementById('like')
+// 	num += 1;
+// 	a.innerText = num + " likes";
+// }
+//
+// function addFavo(){
+// 	alert("Il est bien ajouté dans votre list de favorable.")
+// }
--- test.py
+++ test.py
@@ -42,17 +42,36 @@ def execute_query(connect, query):
 
 @app.route('/', methods=['GET', 'POST'])
 def index_page():
-    if 'logged_in' in session.keys() and session['logged_in']:
-        return render_template('index.html')
-    else:
-        query = f'''select idRegion, nomRegion from Region;'''
-        list_region = select_donne(connection(), query)
-        print(list_region)
-        lieu = [{"ville": "Paris", "duree": "3 Jours, 4 Nuits", "Prix": "1650"},
-                {"ville": "Lille", "duree": "3 Jours, 4 Nuits", "Prix": "1650"},
-                {"ville": "Nice", "duree": "3 Jours, 4 Nuits", "Prix": "1650"},
-                {"ville": "Bordeau", "duree": "3 Jours, 4 Nuits", "Prix": "1650"}]
-        return render_template('index.html', lieu=lieu, region=list_region)
+    query = f'''select idRegion, nomRegion from Region;'''
+    list_region = select_donne(connection(), query)
+    print(list_region)
+    query_lieu_populaire = f'''select idLieu, nomVille, nomLieu, dureeConseillee, prix
+from lieu, ville
+where lieu.idville = ville.idville
+and idLieu in (SELECT idLieu
+FROM liker
+GROUP BY idLieu
+ORDER BY COUNT(*) DESC
+LIMIT 9);
+'''
+    lieu_populaire = select_donne(connection(), query_lieu_populaire)
+    print(lieu_populaire)
+    path = list()
+    for i in lieu_populaire:
+        url_img = f'assets/images/lieu/{i[0]}.jpg'
+        path.append(url_img)
+
+    lieu1 = lieu_populaire[0:4]
+    lieu2 = lieu_populaire[4:8]
+    lieu3 = lieu_populaire[8]
+    path1 = path[0:4]
+    path2 = path[4:8]
+    path3 = path[8]
+    para1 = zip(lieu1, path1)
+    para2 = zip(lieu2, path2)
+    para3 = [lieu3, path3]
+    print(para3)
+    return render_template('index.html', params=para1, p2=para2, p3=para3, region=list_region)
 
 
 @app.route('/destination')
@@ -120,15 +139,57 @@ def detail_page(lieu_id):
     con = connection()
     query = f'''select nomLieu, descLieu, nomVille, idLieu from lieu, ville where lieu.idville = ville.idville and idLieu = {lieu_id};'''
     res = select_donne(con, query)
-    print(res)
-    path = f'assets/images/lieu/{lieu_id}.jpg'
-    print(path)
-    return render_template('detail.html', infos=res, path=path)
+    query_nb_like = f'''select count(idLieu) from liker where idLieu = {lieu_id};'''
 
-#
-# @app.route('/add_favo/<int:lieu_id>')
-# def add_favo(lieu_id):
-#
+    print(res)
+    nb_like = select_donne(connection(), query_nb_like)[0][0]
+    like_flag = False
+    if 'logged_in' in session.keys() and session['logged_in']:
+        query_like = f'''select nb_like from liker where idLieu = {lieu_id} and idUti = {session['userid']};'''
+        like = select_donne(connection(), query_like)
+        if len(like) > 0:
+            like_flag = True
+        else:
+            like_flag = False
+
+    query_commentaire = f'''select idCo, note, descCo from commentaire where idLieu= {lieu_id};'''
+    commentaires = select_donne(connection(), query_commentaire)
+    path = f'assets/images/lieu/{lieu_id}.jpg'
+    print("nubmer", nb_like)
+    return render_template('detail.html', infos=res, path=path, nb=nb_like, like_flag=like_flag, commentaires=commentaires)
+
+
+@app.route('/add_like/<int:lieu_id>')
+def add_like(lieu_id):
+    if 'logged_in' in session.keys() and session['logged_in']:
+        userid = session['userid']
+        query_ajouter_like = f'''insert into liker(idUti, idLieu, nb_like) values ({userid}, {lieu_id}, {1});'''
+        execute_query(connection(), query_ajouter_like)
+        return redirect(url_for('detail_page', lieu_id=lieu_id))
+    else:
+        print('not login')
+        return redirect(url_for("login_page"))
+
+
+@app.route('/add_comm/<int:lieu_id>', methods=['POST'])
+def add_comm(lieu_id):
+    if 'logged_in' in session.keys() and session['logged_in']:
+        userid = session['userid']
+        if request.form.get('com') is not None:
+            comm = request.form.get('com')
+        if request.form.get('note') is not None:
+            note = request.form.get('note')
+        print('enter, ', note)
+        query = f'''SELECT idCo FROM commentaire ORDER BY idCo DESC LIMIT 1;'''
+        id_con = select_donne(connection(), query)
+        idcon = id_con[0][0] + 1
+        query_ajouter_com = f'''insert into commentaire(idCo,descCo,note,idUti,idLieu) values ({idcon}, "{comm}", {note}, {userid}, {lieu_id});'''
+        print(query_ajouter_com)
+        execute_query(connection(), query_ajouter_com)
+        return redirect(url_for('detail_page', lieu_id=lieu_id))
+    else:
+        print('not login')
+        return redirect(url_for("login_page"))
 
 @app.route('/politique')
 def politique_page():
@@ -169,6 +230,19 @@ def logout_page():
     session['logged_in'] = False
     session.pop('username', None)
     return redirect(url_for('index_page'))
+
+
+@app.route('/addfavos/<int:lieu_id>', methods=['GET','POST'])
+def addfavos_page(lieu_id):
+    if 'logged_in' in session.keys() and session['logged_in']:
+        userid = session['userid']
+        query_ajouter_lieu = f'''insert into ajouter(idUti,idLieu) values ({userid}, {lieu_id});'''
+        print(query_ajouter_lieu)
+        execute_query(connection(), query_ajouter_lieu)
+        return redirect(url_for('favos_page'))
+    else:
+        print('not login')
+        return redirect(url_for("login_page"))
 
 
 @app.route('/favos', methods=['GET', 'POST'])
